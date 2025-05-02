ff24ff1ae35845ee2cf067bbd4a52e623fa8fb1f
Jeferson Alvarez <mymotre213@gmail.com>
2025-05-02 13:22:29 -0500

    feat: api completa sin validaciones

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-// Ruta de prueba temporal
-router.get('/', (req, res) => {
-  res.send('');
-});
+router.get('/', userController.getUsers);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
