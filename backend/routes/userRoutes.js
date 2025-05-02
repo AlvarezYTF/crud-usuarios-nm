bfa12e01d4105fa6002d778e024b8d590ed83590
mariocanola <macanola0@soy.sena.edu.co>
2025-05-02 09:48:06 -0500

    organizacion de las rutas

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const upload = require("../middlewares/upload");
+const aprendicesController = require("../controllers/aprendicesController");
 
 // Ruta de prueba temporal
 router.get('/', (req, res) => {
   res.send('');
 });
+
+router.put("/aprendices/:id", upload.single("imagen"), aprendicesController.update);
 
 module.exports = router;
