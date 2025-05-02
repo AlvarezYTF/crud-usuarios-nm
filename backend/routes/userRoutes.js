aea852972f49056a14a42d36fb7c512689cb61c7
Jeferson Alvarez <mymotre213@gmail.com>
2025-05-02 17:07:20 -0500

    feat: verificar correo y contraseña

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const loginController = require('../controllers/loginController');
 
-router.get('/', userController.getUsers);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/users', userController.getUsers);
+router.post('/users', userController.createUser);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
+
+router.post('/login', loginController.login);
 
 module.exports = router;
